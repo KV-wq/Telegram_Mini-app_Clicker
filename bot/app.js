ae489c620cfa4e7b556b15d7dfb1e892e7ee3fd1
KV-wq <refundsix@mail.ru>
2024-10-18 20:43:17 +0700

    Dev commit

---
+++ bot/app.js
@@ -0,0 +1,17 @@
+import { Telegraf, Markup } from "telegraf";
+
+const token = "7647750973:AAHURgvgRwNRZMd4xKaU5ChUD2OaN4e7OyQ";
+const webUrl = "https://kv-clicker.web.app";
+
+const bot = new Telegraf(token);
+
+bot.command("start", (ctx) => {
+  ctx.reply(
+    "Press to start my app",
+    Markup.inlineKeyboard([
+      Markup.button.webApp("Open mini-app 🔥", `${webUrl}?ref=${ctx.payload}`),
+    ])
+  );
+});
+
+bot.launch();
